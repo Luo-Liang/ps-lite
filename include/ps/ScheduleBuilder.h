66c76d398ed3f51935781f4c95aba8b3a6a767d4
Liang Luo <llmainland@live.cn>
2018-03-14 12:12:59 -0700

    smart zips

--- include/ps/ScheduleBuilder.h
+++ include/ps/ScheduleBuilder.h
@@ -19,8 +19,38 @@ public:
 		return results;
 	}
 
-	void IntelligentZip(vector<ScheduleNode> upstream, vector<ScheduleNode> downstream)
+	void IntelligentZip(vector<ScheduleNode>& upstream, vector<ScheduleNode>& downstream)
 	{
+		//the operators must be homogeneous
+		CHECK(upstream.size() == downstream.size());
+		//check that every node is the same operator in upstream.
+		//check that every node is the same operator in downstream.
+		for (Cntr i = 0; i < upstream.size(); i++)
+		{
+			CHECK(upstream.at(i).pOperator->Type == upstream.at(0).pOperator->Type);
+			CHECK(downstream.at(i).pOperator->Type == downstream.at(0).pOperator->Type);
+		}
 
+		//now assign upstream and downstreams accordingly, using RunAt and from, tos.
+		//only connecting send/recv using from tos, others should all be using RunAt.
+		for (Cntr i = 0; i < upstream.size(); i++)
+		{
+			var upstreamType = upstream.at(0).pOperator->Type;
+			//i can infer downstream type to be either recv nodes or other arbitrary nodes.
+			switch (upstreamType)
+			{
+				case OperatorType::PHubBroadcast:
+				{
+					//check everyone is send or is all receive.
+					for (Cntr i = 0; i < upstream.size(); i++)
+					{
+						var ctx = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(upstream.at(i).pContext);
+						var ctx0 = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(upstream.at(0).pContext);
+
+						ctx->
+					}
+				}
+			}
+		}
 	}
 };
