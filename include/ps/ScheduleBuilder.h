ce23f1e599bad2a65d05b6118f460c5b1f8c34a1
Liang Luo <llmainland@live.cn>
2018-03-15 11:39:37 -0700

    more zip ops

--- include/ps/ScheduleBuilder.h
+++ include/ps/ScheduleBuilder.h
@@ -19,7 +19,7 @@ public:
 		return results;
 	}
 
-	void IntelligentZip(vector<ScheduleNode>& upstream, vector<ScheduleNode>& downstream)
+	void IntelligentZip(vector<shared_ptr<ScheduleNode>>& upstream, vector<shared_ptr<ScheduleNode>>& downstream)
 	{
 		CHECK(upstream.size() > 0);
 		CHECK(downstream.size() > 0);
@@ -36,19 +36,19 @@ public:
 		//now assign upstream and downstreams accordingly, using RunAt and from, tos.
 		//only connecting send/recv using from tos, others should all be using RunAt.
 
-		var upstreamType = upstream.at(0).pOperator->Type;
+		var upstreamType = upstream.at(0)->pOperator->Type;
 		switch (upstreamType)
 		{
 			case OperatorType::PHubBroadcast:
 			{
 				//i can infer downstream type to be either recv nodes or other arbitrary nodes.
-				var ctx0 = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(upstream.at(0).pContext);
+				var ctx0 = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(upstream.at(0)->pContext);
 				//check everyone is send or is all receive.
-				var isSender0 = find(ctx0->From.begin(), ctx0->From.end(), upstream.at(0).RunOn) != ctx0->From.end();
+				var isSender0 = find(ctx0->From.begin(), ctx0->From.end(), upstream.at(0)->RunOn) != ctx0->From.end();
 				for (Cntr i = 0; i < upstream.size(); i++)
 				{
-					var ctx = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(upstream.at(i).pContext);
-					var isSender = find(ctx->From.begin(), ctx->From.end(), upstream.at(i).RunOn) != ctx->From.end();
+					var ctx = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(upstream.at(i)->pContext);
+					var isSender = find(ctx->From.begin(), ctx->From.end(), upstream.at(i)->RunOn) != ctx->From.end();
 					CHECK(isSender == isSender0);
 				}
 				//how i zip downstream dependes on whether upstreams are senders.
@@ -57,15 +57,40 @@ public:
 					//upstream are broadcast senders.
 					//downstreams must be broadcast receivers.
 					//check that.
-					var dctx0 = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(downstream.at(0).pContext);
+					CHECK(downstream.at(0)->pOperator->Type == OperatorType::PHubBroadcast);
+					var dctx0 = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(downstream.at(0)->pContext);
 					for (Cntr i = 0; i < downstream.size(); i++)
 					{
-						var ctx = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(downstream.at(i).pContext);
+						var ctx = (shared_ptr<PHubOperatorContext>)dynamic_pointer_cast<PHubOperatorContext>(downstream.at(i)->pContext);
 						var disRecvr = find(ctx->From.begin(), ctx->From.end(), downstream.at(i)->RunOn) == ctx->From.end();
+						CHECK(disRecvr);
+					}
+					//stitch each upstream to a downstream.
+					//if multiple broadcast happening simultaneously, 
+					//make multiple stitches.
+					//unordered_set<shared_ptr<ScheduleNode>> usedDstream;
+					for (Cntr i = 0; i < upstream.size(); i++)
+					{
+						var& current = upstream.at(i);
+						for (var& child : current->Downstream)
+						{
+							//CHECK(usedDstream.find(child) == usedDstream.end());
+							//first find what i am connected to.
+							if (find(downstream.begin(), downstream.end(), child->RunOn) != downstream.end())
+							{
+								//check child is not assigned to anyone.
+								CHECK(child->Upstream.size() == 0);
+								child->Upstream.push_back(current);
+								current->Downstream.push_back(child);
+							}
+							//others dont care
+						}
 					}
 				}
 				else
 				{
+					//i am receievr, so my downstream is solely determined by the runon --- whoever has the same RunOn with me in downstream,
+					//i get to connect with them.
 
 				}
 			}
