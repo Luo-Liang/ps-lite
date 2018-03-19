af9374c6372b1945a1eacad5eaae922c69d299c6
Liang Luo <llmainland@live.cn>
2018-03-18 21:00:15 -0700

    plinksim

--- include/ps/internal/PLinkSim.h
+++ include/ps/internal/PLinkSim.h
@@ -7,19 +7,33 @@ typedef uint DevId;
 class Device
 {
 	//each of the link has one side that is me.
+public:
 	vector<shared_ptr<Link>> Links;
 	DevId DID;
 	NodeId NID;
 };
 
 ///This is what goes into the event driven queue.
+class Link;
 struct PLinkTransferEvent
 {
 	shared_ptr<ScheduleNode> RelevantNode;
 	PLinkEventType EventType;
 	double TimeStamp;
 	double PendingTransfer;
-	shared_ptr<Link> AssignedLink;
+	shared_ptr<vector<shared_ptr<Link>>> AssignedLinks;
+	PLinkTransferEvent(PLinkEventType type, shared_ptr<ScheduleNode> node, double timeStamp, double pendingTransfer, shared_ptr < vector<shared_ptr<Link>>> link)
+	{
+		RelevantNode = node;
+		EventType = type;
+		TimeStamp = timeStamp;
+		PendingTransfer = pendingTransfer;
+		AssignedLinks = link;
+		//for (shared_ptr<Link> assignedLink : *AssignedLinks)
+		//{
+		//	assignedLink->PendingEvents.push_back(this);
+		//}
+	}
 };
 
 class PLinkTransferEventComparer
@@ -46,20 +60,27 @@ public:
 
 class Link
 {
-	uint64_t Bandwidth; //Bps
+public:
+	size_t Bandwidth; //Bps
 	//two endpoints.
 	shared_ptr<Device> EP1;
 	shared_ptr<Device> EP2;
 	//effective bandwidth is Bandwidth, averaged by number of pending events.
-	vector<shared_ptr<PLinkTransferEvent>> PendingEvents;
+	//vector<shared_ptr<PLinkTransferEvent>> PendingEvents;
 };
 
 class Environment
 {
+public:
 	vector<shared_ptr<Device>> Network;
 	//we also need a global route table.
 	//source destination current device -> Link
 	unordered_map<tuple<DevId, DevId, DevId>, shared_ptr<Link>> RouteMap;
+	Environment(vector<shared_ptr<Device>>& networkSetup, unordered_map<tuple<DevId, DevId, DevId>, shared_ptr<Link>>& routeMap)
+	{
+		Network = networkSetup;
+		RouteMap = routeMap;
+	}
 };
 
 enum PLinkEventType
--- src/PLinkSim.cpp
+++ src/PLinkSim.cpp
@@ -1,5 +1,47 @@
 #include <ps/internal/PLinkSim.h>
 #include <queue>
+
+static unordered_map <tuple<DevId, DevId>, shared_ptr<vector<shared_ptr<Link>>>> routeCache;
+static unordered_map<tuple<DevId, DevId>, size_t> bottleneckCache;
+static shared_ptr<vector<shared_ptr<Link>>> GetPath(DevId src, DevId dest, unordered_map<tuple<DevId, DevId, DevId>, shared_ptr<Link>>& routeMap)
+{
+	var key = tuple<DevId, DevId>(src, dest);
+	if (routeCache.find(key) != routeCache.end())
+	{
+		return routeCache.at(key);
+	}
+	//now figure out the new data
+	DevId current = src;
+	var result = make_shared<vector<shared_ptr<Link>>>();
+	uint eBw = UINT_MAX;
+	while (dest != current)
+	{
+		var nextStep = routeMap.at(tuple<DevId, DevId, DevId>(src, dest, current));
+		result->push_back(nextStep);
+		if (nextStep->EP1->DID == current)
+		{
+			current = nextStep->EP2->DID;
+		}
+		else
+		{
+			current = nextStep->EP1->DID;
+		}
+		eBw = eBw > nextStep->Bandwidth ? nextStep->Bandwidth : eBw;
+	}
+	bottleneckCache.at(key) = eBw;
+	routeCache.at(key) = result;
+}
+
+static uint GetLinkBottleneck(DevId src, DevId dest, unordered_map<tuple<DevId, DevId, DevId>, shared_ptr<Link>>& routeMap)
+{
+	var key = tuple<DevId, DevId>(src, dest);
+	if (bottleneckCache.find(key) == bottleneckCache.end())
+	{
+		GetPath(src, dest, routeMap);
+	}
+	return bottleneckCache.at(key);
+}
+
 double PLinkSim::SimulateTime(unordered_map<PLinkKey, size_t>& keySizes,
 	unordered_map<PLinkKey, shared_ptr<Schedule>>& schedules,
 	unordered_map<PLinkKey, double>& ready2GoTime,
@@ -23,7 +65,37 @@ double PLinkSim::SimulateTime(unordered_map<PLinkKey, size_t>& keySizes,
 		var roots = schedule.second->Roots();
 		for (var& root : roots)
 		{
-			//create events.
+			//create events, but only events that are transfer related should I care.
+			var key = schedule.first;
+			var readyTime = ready2GoTime.at(key);
+			//var eType = 
+			var node = root;
+			var pendingTransfer = 0;
+			shared_ptr<Link> lnk = NULL;
+			var size = keySizes.at(key);
+			if (root->pOperator->Type == OperatorType::PHubBroadcast)
+			{
+				pendingTransfer = keySizes.at(key);
+				//which link am i going?
+				var from = root->pContext->From.at(0);
+				for (Cntr i = 0; i < root->pContext->To.size(); i++)
+				{
+					//associated links?
+					var affectedLinks = GetPath(from, root->pContext->To.at(i), env.RouteMap);
+					var projectedEnd = readyTime + size / GetLinkBottleneck(from, root->pContext->To.at(i), env.RouteMap);
+					var eventEndNode = make_shared<PLinkTransferEvent>(PLinkEventType::END, node, readyTime, pendingTransfer, affectedLinks);
+					//queue a 
+
+				}
+			}
+			else if (root->pOperator->Type == OperatorType::GlooCollectiveAlgorithm)
+			{
+
+			}
+			else
+			{
+				//finish instantly
+			}
 
 		}
 	}
